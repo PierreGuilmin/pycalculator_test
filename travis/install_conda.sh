b720bb2175d5bb82b72bf6d983942460957394ce
PierreGuilmin <pierre.guilmin@mines-paristech.fr>
2019-01-12 18:48:26 +0100

    t

--- travis/install_conda.sh
+++ travis/install_conda.sh
@@ -9,24 +9,20 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]] || [[ "$TRAVIS_OS_NAME" == "linux" ]] ; then
     # install Miniconda
     echo "Installing Miniconda..."
     if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda3.sh
     else
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda3.sh
     fi
-    bash miniconda.sh -b -p "$HOME/miniconda3"
+    bash miniconda3.sh -b -p "$HOME/miniconda3"
 
 else
     # see https://conda.io/docs/user-guide/install/windows.html#installing-in-silent-mode
 
     # install Miniconda
     echo "Installing Miniconda..."
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe -O miniconda.exe
+    wget -nv https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe -O miniconda3.exe
 
-    echo "launch start"
-
-    exit 1
-
-    start /wait "" miniconda.exe /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /S /D=%UserProfile%\miniconda3
+    start /wait "" miniconda3.exe /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /S /D=%UserProfile%\miniconda3
 
     echo "done"
 fi
