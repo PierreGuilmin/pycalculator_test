4ce94b1c014fabf90b522999acaf1dec22c23905
PierreGuilmin <pierre.guilmin@mines-paristech.fr>
2019-01-12 19:01:46 +0100

    t

--- travis/install_conda.sh
+++ travis/install_conda.sh
@@ -23,6 +23,7 @@ else
     wget -nv https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe -O miniconda3.exe
 
     echo "start..."
+    ls
 
     start /wait "" miniconda3.exe /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /S /D=%UserProfile%\miniconda3
 
