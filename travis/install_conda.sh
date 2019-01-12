24b25a4714188043e67d794b16384e3c2c860b26
PierreGuilmin <pierre.guilmin@mines-paristech.fr>
2019-01-12 13:35:28 +0100

    ps

--- travis/install_conda.sh
+++ travis/install_conda.sh
@@ -14,7 +14,6 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]] || [[ "$TRAVIS_OS_NAME" == "linux" ]] ; then
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
     fi
     bash miniconda.sh -b -p "$HOME/miniconda3"
-    export PATH="$HOME/miniconda3/bin:$PATH"
 
     # configure Miniconda
     echo "Configuring Miniconda..."
