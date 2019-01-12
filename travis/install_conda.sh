accb59936d89903db5185b6dcb4bd955fb892d71
PierreGuilmin <pierre.guilmin@mines-paristech.fr>
2019-01-12 14:06:16 +0100

    t

--- travis/install_conda.sh
+++ travis/install_conda.sh
@@ -25,10 +25,22 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]] || [[ "$TRAVIS_OS_NAME" == "linux" ]] ; then
     conda info -a
 
 else
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe -O miniconda.exe
+    # see https://conda.io/docs/user-guide/install/windows.html#installing-in-silent-mode
+
     echo "$HOME"
     echo "$PATH"
-    bash miniconda.sh -b -p "$HOME/miniconda3"
-    echo "Oups..."
-    exit 1
+
+    # install Miniconda
+    echo "Installing Miniconda..."
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe -O miniconda.exe
+    start /wait "" miniconda.exe /InstallationType=JustMe /AddToPath=1 /RegisterPython=0 /D=%UserProfile%\Miniconda3
+
+    # configure Miniconda
+    echo "Configuring Miniconda..."
+    conda config --set always_yes true --set changeps1 false
+
+    # update Miniconda
+    echo "Updating Miniconda..."
+    conda update conda
+    conda info -a
 fi
